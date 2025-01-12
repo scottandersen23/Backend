4b3790085ddd7446c3b6e137460c21a0fadfe5b6
Scott Andersen <scottandersen23@gmail.com>
2025-01-12 00:07:41 -0600

    test

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
+class MyTable(models.Model):
+    column1 = models.CharField(max_length=255)
+    column2 = models.IntegerField()
+
+    def __str__(self):
+        return self.column1
 
     
