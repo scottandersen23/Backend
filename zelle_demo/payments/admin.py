7d37d4ecd2ea1e238d8d450aa2f6086977a799d1
Scott Andersen <scottandersen23@gmail.com>
2025-03-02 13:49:52 -0600

    remove all

--- zelle_demo/payments/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Transaction
-
-
-# Register your models here.
-@admin.register(Transaction)
-class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "amount", "status", "create_at", "updated_at")
