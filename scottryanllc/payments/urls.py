d15cb27b51fda75093a08a46073c910c7a5d7b6f
Scott Andersen <scottandersen23@gmail.com>
2025-03-02 15:07:15 -0600

    .

---
+++ scottryanllc/payments/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path    
+from . import views
+
+
+url_patterns = [
+    path("payment/<str:pk>", views.payment, name="payment"),
+    path("payments/", views.payments, name="payments"),
+
+]
--- scottryanllc/payments/views.py
+++ scottryanllc/payments/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def payment(request, pk):
+    return HttpResponse('Searching for Payment_ID: ' + str(pk))
+
+
+def payments(request):
+    return HttpResponse('Here is your payment history.')
