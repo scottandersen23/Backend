4d293e88670b1289518de4aedafc7e578dda77c9
Scott Andersen <scottandersen23@gmail.com>
2025-03-03 22:07:37 -0600

    new data!

--- scottryanllc/payments/views.py
+++ scottryanllc/payments/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Payment
 
 paymentsList = [{'id': '1','title': "Payments Website", 'description': 'Full Stack Django Project'},{'id': '2','title': "Blog", 'description': 'Developing Software Applications'},{'id': '3','title': "Personal Portfolio", 'description': 'Professional Projects Folder'}]
 
 def payment(request, pk):
-    paymentsObj = None
-    for i in paymentsList:
-        if i['id'] == pk:
-            paymentsObj = i
-    return render(request, 'payments/single-payments.html', {'payment':paymentsObj})
+    paymentObj = Payment.objects.get(id=pk)
+    tags = paymentObj.tags.all()
+    print('paymentObj', paymentObj)
+    return render(request, 'payments/single-payments.html', {'payment':paymentObj, 'tags': tags})
 
 
 def payments(request):
-    page = 'payments'
-    number = 10
-    context = {'page': page, 'number': number, 'payments': paymentsList}
+    payments = Payment.objects.all()
+    context = {'payments': payments}
     return render(request, 'payments/payments.html', context)
