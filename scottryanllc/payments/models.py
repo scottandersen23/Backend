c971468e287124bc2b9e987e337a9c1a7ddf5132
Scott Andersen <scottandersen23@gmail.com>
2025-03-03 19:46:00 -0600

    first model db table

--- scottryanllc/payments/models.py
+++ scottryanllc/payments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+# Payment Table
+class Payment(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) # blank here allows us to have a null field in a form.
+    demo_link = models.CharField(max_length=2000, null=True, blank=True) # when NULL is True, the col doesn't require data.
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return self.title
