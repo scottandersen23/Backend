3ac23511047f305f350ffd8cb68a983695c4d2ef
Scott Andersen <scottandersen23@gmail.com>
2025-03-03 21:08:19 -0600

    building tables

--- scottryanllc/payments/admin.py
+++ scottryanllc/payments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Payment
+from .models import Payment, Tag
 
 admin.site.register(Payment)
+admin.site.register(Tag)
--- scottryanllc/payments/models.py
+++ scottryanllc/payments/models.py
@@ -7,9 +7,19 @@ class Payment(models.Model):
     description = models.TextField(null=True, blank=True) # blank here allows us to have a null field in a form.
     demo_link = models.CharField(max_length=2000, null=True, blank=True) # when NULL is True, the col doesn't require data.
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
 
     def __str__(self):
         return self.title
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.name
